880efd74ad7946bbc19dba5dc78c6eaa6c08e66e
itdanja <itdanja@kakao.com>
2024-07-12 11:53:40 +0900

    day07  @ResponseBody

--- src/main/java/example/day07/restcontroller/RestController2.java
+++ src/main/java/example/day07/restcontroller/RestController2.java
@@ -1,10 +1,13 @@
 package example.day07.restcontroller;
 
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
 
 @Controller
 public class RestController2 {
@@ -22,13 +25,57 @@ public class RestController2 {
     // [2] HTTP POST
     @RequestMapping( value = "/example/rest2" , method = RequestMethod.POST )
     @ResponseBody    // 응답할 데이터의 자바타입을 HTTP타입으로 자동 타입 설정 : (java) [] -> (HTTP) application/json;
-    public String[] getPost2( HttpServletRequest request ){
+    //public String[] getPost2( HttpServletRequest request ){
+    //public ArrayList<String> getPost2( HttpServletRequest request ){
+    //public RestDto getPost2( HttpServletRequest request ){
+    public ArrayList< RestDto > getPost2( HttpServletRequest request ){
         System.out.println("RestController2.getPost2");
+        String key = request.getParameter("key");   System.out.println("key = " + key);
+        // (1) 배열[] 타입      -> [ "[POST]","ClientHi" ]
+        // String[] strArray = new String[2];
+        // strArray[0] = "[POST]"; strArray[1] ="ClientHi";
+        // (2) 리스트 타입       -> [ "[POST]","ClientHi" ]
+        // ArrayList<String> strArray = new ArrayList<>();
+        // strArray.add("[POST]"); strArray.add("ClientHi");
+        // (3) 객체(DTO) 타입   -> {  "key1": "[POST]", "key2": "ClientHi"  }
+        // RestDto strArray = new RestDto("[POST]" , "ClientHi");
+        // (4) 리스트 안에 DTO 타입 -> [ { "key1": "[POST1]", "key2": "ClientHi1" }, { "key1": "[POST2]","key2": "ClientHi2"} ]
+        ArrayList< RestDto > strArray = new ArrayList<>();
+        strArray.add( new RestDto("[POST1]" , "ClientHi1") );
+        strArray.add( new RestDto("[POST2]" , "ClientHi2") );
+        return strArray;
+    }
+    // [3] HTTP PUT
+    @RequestMapping(value = "/example/rest2" , method = RequestMethod.PUT)
+    @ResponseBody
+    public int putRest2( HttpServletRequest request ){
+        System.out.println("RestController2.putRest2");
         String key = request.getParameter("key");
         System.out.println("key = " + key);
-        // 데이터 구성
-        String[] strArray = new String[2];
-        strArray[0] = "[POST]"; strArray[1] ="ClientHi";
-        return strArray;        // JSON : [ "[POST]","ClientHi" ]
+        return 10+10; // application/json
+    }
+
+    // [4] HTTP delete
+    @RequestMapping( value = "/example/rest2" , method = RequestMethod.DELETE)
+    @ResponseBody
+    public boolean deleteRest2(HttpServletRequest request ){
+        System.out.println("RestController2.deleteRest2");
+        String key = request.getParameter("key");
+        System.out.println("key = " + key);
+        return true; // application/json
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
