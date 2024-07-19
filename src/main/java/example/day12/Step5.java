9e02627e96844805bbc9ce220e6fd6d13552bbf2
itdanja <itdanja@kakao.com>
2024-07-19 12:59:02 +0900

    day12 컬렉션실습

--- src/main/java/example/day12/Step5.java
+++ src/main/java/example/day12/Step5.java
@@ -2,10 +2,10 @@ package example.day12;
 
 import java.util.*;
 
-public class Step4 {
+public class Step5 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Set< User > set = new HashSet<>();
+        Map< String , Integer > map = new HashMap<>();
         while(true){
             System.out.print("1.등록 2.전체출력 3.수정 4.삭제 ");
             int ch = scan.nextInt();
@@ -13,26 +13,24 @@ public class Step4 {
                 System.out.print(" 이름 : "); String name = scan.next();
                 System.out.print(" 나이 : "); int age = scan.nextInt();
                 User user = new User();  user.name = name ; user.age = age;
-                set.add( user );
+                map.put( name , age );
             }
             if( ch ==2 ){
-                set.forEach( user -> {  System.out.printf( "%s\t%d\n" , user.name , user.age );  });
+                map.keySet().forEach( key -> {  System.out.printf( "%s\t%d\n" , key , map.get( key ) );  });
             }
             if( ch ==3 ){
                 System.out.print("수정할 이름 :"); String name = scan.next();
-                set.forEach( user ->{
-                    if( user.name.equals( name ) ){
-                        System.out.print("수정할 나이");   int age = scan.nextInt();  user.age = age;
+                map.keySet().forEach( key -> {
+                    if( key.equals( name ) ){
+                        System.out.print("수정할 나이 : "); int age = scan.nextInt();
+                        map.put( key , age );
+                        // map.replace( key , age ); // 수정
                     }
                 });
             }
             if( ch ==4 ){
                 System.out.print("삭제할 이름 :"); String name = scan.next();
-                set.forEach( user -> {
-                    if( user.name.equals( name ) ){
-                        set.remove( user );
-                    }
-                });
+                map.remove( name );
             }
         } // w end
     } // main emd
